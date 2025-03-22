15df29940058befd3b4889f100eb0c33a3188ccb
nikhilc52 <nsc49@cornell.edu>
2025-03-22 00:44:06 -0400

    changed implementation to scrollyvideo.js

---
+++ website/next.js
@@ -0,0 +1,13 @@
+// standard starting stuff
+const next = document.getElementById("next");
+next.style.visibility = 'hidden';
+
+window.addEventListener('scroll', () => {  
+  // how far has the user scrolled 
+  const scrollTop = html.scrollTop;
+  // maximum that the user can scroll inside the current window
+  const maxScrollTop = html.scrollHeight - window.innerHeight
+  const scrollFraction = scrollTop / maxScrollTop;
+  // when hits half way then opacity is 1 otherwise opacity moves towards 0
+  next.style.visibility = scrollFraction >= .99 ? 'visible' : 'hidden';
+});
