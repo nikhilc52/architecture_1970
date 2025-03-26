1243e5068daf98978f82b8ca834529aa94519129
nikhilc52 <nsc49@cornell.edu>
2025-03-25 23:51:39 -0400

    added part/7 and scroll to begin

--- website/loading.js
+++ website/loading.js
@@ -9,34 +9,49 @@ const title = document.getElementById("title");
 title.style.visibility = 'visible'
 const desc = document.getElementById("desc");
 desc.style.visibility = 'visible'
+const scroll_to_begin = document.getElementById("scroll");
+scroll_to_begin.style.visibility = 'hidden'
 
-// repeat the task for 20 secs
-for (let i = 0; i < 20; i++){
-    task(i);
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// repeat the task for 20 secs, waiting for the termination of the function each time
+async function run() {
+    for (let i = 0; i < 20; i++) {
+        await task(i)
+        if (scroll_to_begin.style.visibility == 'visible'){break}
+    }
+}
+
 
 // helper function
 function randomIntFromInterval(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function task(i) {
+async function task(i) {
+    await delay(1000)
     // every second do this
-    setTimeout(function () {
-        // if we reached the end of the loading screen
-        if (i == 19) { loading.innerText = "Loading ... 100.0%"
-            // stall on 100% loading for a second
-            setTimeout(() => {
-                loading.style.visibility = 'hidden'
-                desc.style.visibility = 'hidden'
-                title.style.visibility = 'hidden'
-                video.style.visibility = 'visible'
-              }, "1000"); }
-        else {
+    // if we reached the end of the loading screen
+    if (i == 19) {
+        loading.innerText = "Loading ... 100.0%"
+        // stall on 100% loading for a second
+        setTimeout(() => {
+            loading.style.visibility = 'hidden'
+            desc.style.visibility = 'hidden'
+            title.style.visibility = 'hidden'
+            video.style.visibility = 'visible'
+            scroll_to_begin.style.visibility = 'visible'
+        }, "1000");
+    }
+    else {
         // get the only canvas element in the DOM
-        try{const canvas = document.getElementsByTagName('canvas')[0];
+        try {
+            const canvas = document.getElementsByTagName('canvas')[0];
             // if a random attribute is null, then it exists
-            if (canvas.getAttribute("listener") == null){
+            if (canvas.getAttribute("listener") == null) {
                 loading.innerText = "Loading ... 100.0%"
                 // stall on 100% loading for a second
                 setTimeout(() => {
@@ -44,11 +59,15 @@ function task(i) {
                     desc.style.visibility = 'hidden'
                     title.style.visibility = 'hidden'
                     video.style.visibility = 'visible'
-                  }, "1000");
-            }}
-            // if there is no canvas element (not done loading video)
-        catch (e){
-            loading.innerText = ("Loading ... " + i*5 + "." + randomIntFromInterval(0,9) + "%")
-        }}
-    },1000*i);
+                    scroll_to_begin.style.visibility = 'visible'
+                }, "1000");
+            }
+        }
+        // if there is no canvas element (not done loading video)
+        catch (e) {
+            loading.innerText = ("Loading ... " + i * 5 + "." + randomIntFromInterval(0, 9) + "%")
+        }
+    }
 }
+
+run()
--- website/next.js
+++ website/next.js
@@ -6,9 +6,14 @@ next.style.visibility = 'hidden';
 window.addEventListener('scroll', () => {  
   // how far has the user scrolled 
   const scrollTop = html.scrollTop;
+  if (window.scrollY > 10){
+    scroll_to_begin.style.visibility = 'hidden'
+  } 
   // maximum that the user can scroll inside the current window
   const maxScrollTop = html.scrollHeight - window.innerHeight
   const scrollFraction = scrollTop / maxScrollTop;
   // when hits half way then opacity is 1 otherwise opacity moves towards 0
   next.style.visibility = scrollFraction >= .99 ? 'visible' : 'hidden';
 });
+
+window.add
