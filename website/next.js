0495f574fad2e4ceeca15c1255637627e64c2963
nikhilc52 <nsc49@cornell.edu>
2025-04-12 04:48:38 -0400

    Changed resize to be hidden after scrolling
    - Fixed background images
    - Moved starting tags

--- website/next.js
+++ website/next.js
@@ -3,11 +3,15 @@ const html = document.documentElement;
 const next = document.getElementById("next");
 next.style.visibility = 'hidden';
 
+const resize = document.getElementById("resize");
+
 window.addEventListener('scroll', () => {  
   // how far has the user scrolled 
   const scrollTop = html.scrollTop;
   if (window.scrollY > 10){
     scroll_to_begin.style.visibility = 'hidden'
+    // gets rid of resize on any scroll, since initially hidden
+    resize.style.visibility = 'hidden'
   } 
   // maximum that the user can scroll inside the current window
   const maxScrollTop = html.scrollHeight - window.innerHeight
